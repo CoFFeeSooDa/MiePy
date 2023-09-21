9ecd1f1ed35716175e91808d0c610117251fa7bf
CoFFeeSooDa <mingwei.wayne.lee@gmail.com>
2023-09-21 14:40:19 +0800

    create function of Mie coefficients

---
+++ functions/mie_coefficient.py
@@ -0,0 +1,25 @@
+import numpy as np
+import basis_function as bf
+
+def mie_single(calculation_details:list, log_message:None):
+    """mie_single
+
+    Args:
+        calculation_details (list):
+            -----------------------------------------------
+            key
+            ks
+            nr
+            nmax
+        log_message (object): object of logging standard module (for the use of MiePy logging only)
+        
+    Returns:
+        Tuple: Normalized M and N functions
+        M (ndarray[float], n x (2n+1) x 3): vector spherical function M
+        N (ndarray[float], n x (2n+1) x 3): vector spherical function N
+        
+    """
+
+
+if __name__ == '__main__':
+    print('success')
